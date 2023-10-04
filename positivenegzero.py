9244ffec779a7d42b71d8236a741a74a687243ef
arjunpradeepp <146503880+arjunpradeepp@users.noreply.github.com>
2023-10-04 09:58:18 +0530

    Create positivenegzero.py

---
+++ positivenegzero.py
@@ -0,0 +1,7 @@
+num=float(input("enter the number"))
+if num>0:
+    print("The number is positive number")
+elif num==0:
+    print("The number is zero")
+else:
+    print("The number is a negative number")
